bb58299d3d3bbe70a2eff57e98803e87180da146
LydiaMamoade <malyf2002@gmail.com>
2018-01-11 13:24:32 +0100

    Modif 13:24

--- projet_2/GrandHotel/Contexte.cs
+++ projet_2/GrandHotel/Contexte.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using GrandHotel.Properties;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GrandHotel
 {
-    class Contexte
+    public class Contexte
     {
+       
+//        public static List<Facture> GetFactures(DateTime date)
+//        {
+//            var liste = new List<Facture>();
+//            var cmd = new SqlCommand();
+//            cmd.CommandText = @"select Id,IdClient,DateFacture,CodeModePaiement
+//from Facture
+//where DateFacture='@date'";
+
+//            cmd.Parameters.Add(new SqlParameter
+//            {
+//                SqlDbType = SqlDbType.Date,
+//                ParameterName = "@date",
+//                Value = date
+//            });
+
+//            using (var conn = new SqlConnection(Settings1.Default.GrandHotelConnexion))
+//            {
+//                cmd.Connection = conn;
+//                conn.Open();
+
+//                using (SqlDataReader reader = cmd.ExecuteReader())
+//                {
+//                    while (reader.Read())
+//                    {
+//                        var item = new Facture();
+//                        item.Id = (int)reader["Id"];
+//                        item.IdClient = (int)reader["IdClient"];
+//                        item.DateFacture = (DateTime)reader["DateFacture"];
+//                        item.CodeModePaiement = (string)reader["DateFacture"];
+//                        liste.Add(item);
+//                    }
+//                }
+//            }
+
+//            return liste;
+//        }
     }
 }
+
--- projet_2/GrandHotel/Pages/PageFacture.cs
+++ projet_2/GrandHotel/Pages/PageFacture.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Outils.TConsole;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace GrandHotel.Pages
 {
     class PageFacture
     {
+        public PageFacture()
+        {
+            //Menu.AddOption("1", "Liste des factures", () => AfficherFactures());
+                     
+        }
+
+        //private void AfficherFactures()
+        //{
+        //    string liste = Input.Read<string>("Saisir la date de la facture");
+        //    ConsoleTable.From(liste).Display("Factures");
+        //}
     }
 }
